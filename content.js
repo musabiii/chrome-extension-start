b15ad918ad5fde292613881616b9fa473034793b
musabiii <musabi@live.com>
2022-02-06 04:16:41 +0500

    fix local storage

--- content.js
+++ content.js
@@ -3,19 +3,20 @@ const video = document.querySelector("video");
 let extActive = false;
 
 document.addEventListener("mouseleave", function (e) {
-    if (extActive) {
-        video.pause();
-        console.log('pause');
-    }
+  if (extActive) {
+    video.pause();
+    console.log("pause");
+  }
 });
 
 document.addEventListener("mouseenter", function (e) {
-    if (extActive) {
-        video.play();
-        console.log('play');
-    }
+  if (extActive) {
+    video.play();
+    console.log("play");
+  }
 });
 
-chrome.runtime.onMessage.addListener((req)=>{
-    extActive = req;
-})
+chrome.runtime.onMessage.addListener((req) => {
+  extActive = req;
+});
+
--- popup.js
+++ popup.js
@@ -1,15 +1,21 @@
-console.log("s");
-// console.log(chrome.bookmarks);
+const extActive = localStorage.getItem("ExtensionActive");
 const checkbox = document.querySelector("#checkbox");
+
+if ((extActive === "true")) {
+  checkbox.checked = true;
+} else {
+  checkbox.checked = false;
+}
 console.log(checkbox);
 checkbox.addEventListener("click", (e) => {
-
   chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
-      console.log(tabs);
+    console.log(tabs);
     chrome.tabs.sendMessage(tabs[0].id, checkbox.checked);
+  localStorage.setItem("ExtensionActive", checkbox.checked + "");
+
   });
 
   console.log(checkbox.checked);
 });
 
-console.log('open');
+console.log("open");
