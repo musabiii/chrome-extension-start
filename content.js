c084d72f4c6396af52251ef1adc9b30fc042e210
musabiii <musabi@live.com>
2022-02-06 03:34:50 +0500

    worked

--- content.js
+++ content.js
@@ -1,9 +1,21 @@
 const video = document.querySelector("video");
 
+let extActive = false;
+
 document.addEventListener("mouseleave", function (e) {
-  video.pause();
+    if (extActive) {
+        video.pause();
+        console.log('pause');
+    }
 });
 
 document.addEventListener("mouseenter", function (e) {
-  video.play();
+    if (extActive) {
+        video.play();
+        console.log('play');
+    }
 });
+
+chrome.runtime.onMessage.addListener((req)=>{
+    extActive = req;
+})
--- popup.js
+++ popup.js
@@ -1,2 +1,15 @@
-console.log('s');
-console.log(chrome.bookmarks);
+console.log("s");
+// console.log(chrome.bookmarks);
+const checkbox = document.querySelector("#checkbox");
+console.log(checkbox);
+checkbox.addEventListener("click", (e) => {
+
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+      console.log(tabs);
+    chrome.tabs.sendMessage(tabs[0].id, checkbox.checked);
+  });
+
+  console.log(checkbox.checked);
+});
+
+console.log('open');
