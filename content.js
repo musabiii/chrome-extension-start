8f3c7b81610aae05c25403c9d0a84488c5f4c3bf
musabiii <musabi@live.com>
2022-02-06 23:54:53 +0500

    move button

--- content.js
+++ content.js
@@ -1,18 +1,61 @@
+
+const flex = document.querySelector('.ytp-left-controls');
+const checkbox = document.createElement('input');
+
+const checkboxContainer = document.createElement('div');
+checkboxContainer.classList.add('checkbox-container');
+
+// const pic = document.createElement('img');
+// pic.classList.add('checkbox-pic');
+// pic.src = './icon.png';
+
+
+const boxCkeck = document.createElement('div');
+boxCkeck.classList.add('box-check');
+
+const boxCircle = document.createElement('div');
+boxCircle.classList.add('box-circle');
+
+// checkboxContainer.insertAdjacentElement('afterbegin',pic);
+
+boxCkeck.insertAdjacentElement('afterbegin',boxCircle);
+checkboxContainer.insertAdjacentElement('afterbegin',boxCkeck);
+flex.insertAdjacentElement('beforeend',checkboxContainer);
+
+
+
+boxCkeck.addEventListener("click", () => {
+    checkbox.checked = !boxCkeck.classList.contains('active');
+    boxCkeck.classList.toggle("active");
+});
+
+
+
+
+// const label = document.createElement('label');
+// label.innerText = 'Mouse play'
+// const div = document.createElement('input');
+// div.type = 'checkbox';
+// div.style.cursor = 'pointer';
+// label.style.cursor = 'pointer';
+// label.insertAdjacentElement('afterbegin',div);
+// flex.insertAdjacentElement('beforeend',label);
+
+// div.checked
+
 const video = document.querySelector("video");
 
 let extActive = false;
 
 document.addEventListener("mouseleave", function (e) {
-  if (extActive) {
+  if (checkbox.checked) {
     video.pause();
-    console.log("pause");
   }
 });
 
 document.addEventListener("mouseenter", function (e) {
-  if (extActive) {
+  if (checkbox.checked) {
     video.play();
-    console.log("play");
   }
 });
 
